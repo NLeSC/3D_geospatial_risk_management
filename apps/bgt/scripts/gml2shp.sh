82f7a440731dd0e9274612c73569dff1c7923f4c
Tom van Tilburg <tom.van.tilburg@gmail.com>
2016-04-01 15:45:17 +0200

    Update gml2shp.sh

--- apps/bgt/scripts/gml2shp.sh
+++ apps/bgt/scripts/gml2shp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-for f in `ls extract`; do d=${f%.*}; echo ${d}; ./gml2shp.py extract/$d.gml outShp/$d.shp; done
+#for f in `ls extract`; do d=${f%.*}; echo ${d}; ./gml2shp.py extract/$d.gml outShp/$d.shp; done
+for f in `ls extract`; do d=${f%.*}; echo ${d}; ogr2ogr -f csv -lco GEOEMTRY=AS_WKT outShp/$d.csv extract/$d.gml ; done
