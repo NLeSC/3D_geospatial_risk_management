deda674916a6b058cacf23c99a8fd594f708e5a5
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-04-05 17:00:56 +0200

    remove swp file

--- apps/x3d_viewer/queries/test_buildings.sql
+++ apps/x3d_viewer/queries/test_buildings.sql
@@ -7,38 +7,44 @@ set _south = 93916;
 set _east  = 463891;
 set _north = 463991;
 
+create table ahn3T (x decimal(9,3), y decimal(9,3), z decimal(9,3), a int, i int, n int, r int, c int, p int, e int, d int, M int);
+
 drop table bounds;
 create table bounds AS 	SELECT ST_MakeEnvelope(_west, _south, _east, _north, 28992) as geom with data;
 
 drop table pointcloud;
 create table pointcloud AS (
-	SELECT PC_FilterEquals(pa,'classification',6) pa 
-	FROM ahn3_pointcloud.vw_ahn3, bounds 
-	WHERE ST_DWithin(geom, Geometry(pa),10) --patches should be INSIDE bounds
+	SELECT x, y, z 
+	FROM ahn3T, bounds 
+	WHERE 
+	--ST_DWithin(geom, Geometry(pa),10) --patches should be INSIDE bounds
+	ST_DWithin(geom, ST_geomFromText('Point( 10.0 11.0 12.0)', 28992), 10) --patches should be INSIDE bounds
+	--classification = 6
+	and c = 6
 ) with data;
 
 drop table footprints;
 create table footprints AS (
-	SELECT ST_Force3D(ST_GeometryN(ST_SimplifyPreserveTopology(geometrie2dgrondvlak,0.4),1)) geom,
-	a.ogc_fid id,
-	0 bouwjaar
-	FROM bgt_import.buildingpart a, bounds b
+	SELECT ST_Force3D(ST_GeometryN(kmlgeometry,1)) as geom,
+	a.gml_id as id,
+	0 as bouwjaar
+	FROM bgt_buildingpart a, bounds b
 	WHERE 1 = 1
 	--AND a.ogc_fid = 688393 --DEBUG
 	--AND bgt_status = 'bestaand'
-	AND ST_Area(a.geometrie2dgrondvlak) > 30
-	AND ST_Intersects(a.geometrie2dgrondvlak, b.geom)
-	AND ST_Intersects(ST_Centroid(a.geometrie2dgrondvlak), b.geom)
-	AND ST_IsValid(a.geometrie2dgrondvlak)
-    --AND ST_GeometryType(a.geometrie2dgrondvlak) = 'ST_MultiPolygon'
+	AND ST_Area(a.kmlgeometry) > 30
+	AND ST_Intersects(a.kmlgeometry, b.geom)
+	AND ST_Intersects(ST_Centroid(a.kmlgeometry), b.geom)
+	AND ST_IsValid(a.kmlgeometry)
+    --AND ST_GeometryType(a.kmlgeometry) = 'ST_MultiPolygon'
 ) with data;
 
 drop table papoints;
 create table papoints AS ( --get points from intersecting patches
 	SELECT 
 		a.id,
-		PC_Explode(b.pa) pt,
-		geom footprint
+		PC_Explode(b.pa) as pt,
+		geom as footprint
 	FROM footprints a
 	LEFT JOIN pointcloud b ON (ST_Intersects(a.geom, geometry(b.pa)))
 ) with data;
