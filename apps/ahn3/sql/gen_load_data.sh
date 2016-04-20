efa42d080b10b92f242de8a3515b1948e3f88606
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-04-20 14:57:33 +0200

    Improve script and fix the attributes types for binary data loading

--- apps/ahn3/sql/gen_load_data.sh
+++ apps/ahn3/sql/gen_load_data.sh
@@ -30,22 +30,44 @@ data_path=$ahn3_data_dir
 echo "AHN3 LAS/LAZ files are located at: $data_path" >&2
 echo ""
 
-echo "create merge table ahn3 (x decimal(9,3), y decimal(9,3), z decimal(9,3), a int, i int, n int, r int, c int, p int, e int, d int, M int);" > "create_merge_table.sql"
+for f in `ls $data_path | grep -E LAZ | sed 's/\.LAZ//g'`; do
+    echo "create table $f (x decimal(9,3), y decimal(9,3), z decimal(9,3), a tinyint, i smallint, n smallint, r smallint, c tinyint, p smallint, e smallint, d smallint, M int);";
+done > "create_tables.sql"
+echo "Defining the merge table done..."
+
+echo "create merge table ahn3 (x decimal(9,3), y decimal(9,3), z decimal(9,3), a tinyint, i smallint, n smallint, r smallint, c tinyint, p smallint, e smallint, d smallint, M int);" > "create_merge_table.sql"
 for f in `ls $data_path | grep -E LAZ | sed 's/\.LAZ//g'`; do
 	echo "alter table ahn3 add table $f;";
 done >> "create_merge_table.sql"
 echo "Defining the merge table done..."
 
-echo "drop table ahn3;"
+echo "drop table ahn3;" > "drop_tables.sql"
 for f in `ls $data_path | grep -E LAZ | sed 's/\.LAZ//g'`; do
 	echo "drop table $f;";
 done >> "drop_tables.sql"
 echo "Defining drop tables done..."
 
 for f in `ls $data_path | grep -E LAZ | sed 's/\.LAZ//g'`; do
+	echo "select '$f', count(*) from $f;";
+done > "count_tables.sql"
+echo "Defining count tables done..."
+
+for f in `ls $data_path | grep -E LAZ | sed 's/\.LAZ//g'`; do
+	echo "alter table $f set read only;";
+done > "set_readonlys.sql"
+echo "Defining iset read only done..."
+
+for f in `ls $data_path | grep -E LAZ | sed 's/\.LAZ//g'`; do
 	echo "call lidarattach('$data_path/$f.LAZ');";
 done > "attach_data.sql"
 echo "Defining data attachment done..."
+
+for f in `ls $data_path | grep -E LAZ | sed 's/\.LAZ//g'`; do
+	echo "copy binary into $f from('$data_path/../outs/$f\_out_col_X.dat', '$data_path/../outs/$f\_out_col_Y.dat', '$data_path/../outs/$f\_out_col_Z.dat', '$data_path/../outs/$f\_out_col_a.dat', '$data_path/../outs/$f\_out_col_i.dat', '$data_path/../outs/$f\_out_col_n.dat', '$data_path/../outs/$f\_out_col_r.dat', '$data_path/../outs/$f\_out_col_c.dat', '$data_path/../outs/$f\_out_col_p.dat', '$data_path/../outs/$f\_out_col_e.dat', '$data_path/../outs/$f\_out_col_d.dat', '$data_path/../outs/$f\_out_col_M.dat');";
+done > "load_data.sql"
+echo "Defining data attachment done..."
+
+sed -i 's/\\_out/_out/g' load_data.sql
 
 echo ""
 echo "The DDLs were generated with success please follow the following steps:"
