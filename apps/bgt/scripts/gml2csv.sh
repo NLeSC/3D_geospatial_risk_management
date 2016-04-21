4c3aeb4fb621389a124c28739887d591dc233f71
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-04-21 12:21:23 +0200

    Use the right script name for its task

---
+++ apps/bgt/scripts/gml2csv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for f in `ls extract`; do d=${f%.*}; echo ${d}; ogr2ogr -f csv -lco GEOEMTRY=AS_WKT outCsv/$d.csv extract/$d.gml ; done
+
+sed -i 's/",\(\w\)/"\nclean,\1/g' *.csv
+sed -i 's/,"/,\nmerge"/g' *.csv
+sed -i 's/",/"\nclean,/g' *.csv
+sed -i '/^clean/s/,/|/g' *.csv
+sed -e ':a' -e 'N' -e '$!ba' -i -e 's/\nmerge//g' *.csv
+sed -e ':a' -e 'N' -e '$!ba' -i -e 's/\nclean//g' *.csv
+sed -i 's/"//g' *.csv
+sed -i 's/|false|/0/g' *.csv
+sed -i 's/|true|/1/g' *.csv
