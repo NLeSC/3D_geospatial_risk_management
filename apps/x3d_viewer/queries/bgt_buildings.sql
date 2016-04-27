dc8e7ee54010d5963717c49e5068fefd2d058398
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-04-27 14:52:38 +0200

    Convert and make it work in MonetDB. First version working.

--- apps/x3d_viewer/queries/bgt_buildings.sql
+++ apps/x3d_viewer/queries/bgt_buildings.sql
@@ -1,45 +1,56 @@
+declare _west integer;
+declare _south integer;
+declare _east integer;
+declare _north integer;
+set _west = 93816;
+set _east = 93916;
+set _south = 463891;
+set _north = 463991;
+
 WITH 
 bounds AS (
-	--SELECT ST_Buffer(ST_Transform(ST_SetSrid(ST_MakePoint(_lon, _lat),4326), 28992),200) geom
-	SELECT ST_MakeEnvelope(_west, _south, _east, _north, 28992) geom
+    SELECT ST_SetSRID(ST_MakeEnvelope(_west, _south, _east, _north, 28992), 4326) as geom 
 ), 
 pointcloud AS (
-	SELECT PC_FilterEquals(pa,'classification',6) pa 
-	FROM ahn3_pointcloud.vw_ahn3, bounds 
-	WHERE ST_DWithin(geom, Geometry(pa),10) --patches should be INSIDE bounds
+	SELECT x, y, z 
+	FROM ahn3, bounds 
+	WHERE 
+    --x between _west and _east and
+    --y between _south and _north and
+    x between 93816 and 93916 and
+    y between 463891 and 463991 and
+	ST_DWithin(geom, ST_SetSRID(ST_MakePoint(x, y, z), 4326), 10)
+	and c = 6
 ),
 footprints AS (
-	SELECT ST_Force3D(ST_GeometryN(ST_SimplifyPreserveTopology(geometrie2dgrondvlak,0.4),1)) geom,
-	a.ogc_fid id,
-	0 bouwjaar
-	FROM bgt_import.buildingpart a, bounds b
+	--SELECT ST_Force3D(ST_GeometryN(wkt,1)) as geom,
+	SELECT ST_GeometryN(wkt,1) as geom,
+	a.gml_id as id,
+	0 as bouwjaar
+	FROM bgt_buildingpart a, bounds b
 	WHERE 1 = 1
-	--AND a.ogc_fid = 688393 --DEBUG
-	--AND bgt_status = 'bestaand'
-	AND ST_Area(a.geometrie2dgrondvlak) > 30
-	AND ST_Intersects(a.geometrie2dgrondvlak, b.geom)
-	AND ST_Intersects(ST_Centroid(a.geometrie2dgrondvlak), b.geom)
-	AND ST_IsValid(a.geometrie2dgrondvlak)
-    --AND ST_GeometryType(a.geometrie2dgrondvlak) = 'ST_MultiPolygon'
+	AND ST_Area(a.wkt) > 30
+	AND ST_Intersects(ST_SetSRID(a.wkt, 4326), b.geom)
+	AND ST_Intersects(ST_SetSRID(ST_Centroid(a.wkt), 4326), b.geom)
+	AND ST_IsValid(a.wkt)
 ),
-papoints AS ( --get points from intersecting patches
-	SELECT 
-		a.id,
-		PC_Explode(b.pa) pt,
-		geom footprint
-	FROM footprints a
-	LEFT JOIN pointcloud b ON (ST_Intersects(a.geom, geometry(b.pa)))
-),
+--papoints AS ( --get points from intersecting patches
+--	SELECT 
+--		a.id,
+--		PC_Explode(b.pa) pt,
+--		geom footprint
+--	FROM footprints a
+--	LEFT JOIN pointcloud b ON (ST_Intersects(a.geom, geometry(b.pa)))
+--),
 stats_fast AS (
 	SELECT 
-		PC_PatchAvg(PC_Union(pa),'z') max,
-		PC_PatchMin(PC_Union(pa),'z') min,
 		footprints.id,
 		bouwjaar,
-		geom footprint
-	FROM footprints 
-	--LEFT JOIN ahn_pointcloud.ahn2objects ON (ST_Intersects(geom, geometry(pa)))
-	LEFT JOIN pointcloud ON (ST_Intersects(geom, geometry(pa)))
+		geom as footprint,
+        max(z) as max,
+        min(z) as min
+	FROM footprints, pointcloud
+    WHERE ST_Intersects(geom, ST_MakePoint(x, y, z))
 	GROUP BY footprints.id, footprint, bouwjaar
 ),
 polygons AS (
@@ -49,12 +60,7 @@ polygons AS (
 			ST_Extrude(
 				ST_Translate(footprint,0,0, min)
 			, 0,0,max-min -2)
-		) 
-		geom FROM stats_fast
-	--SELECT ST_Tesselate(ST_Translate(footprint,0,0, min + 20)) geom FROM stats_fast
+		) as geom
+        FROM stats_fast
 )
-SELECT id,
---s.type as type,
-'building' as type,
-'red' color, ST_AsX3D((p.geom)) geom
-FROM polygons p;
+SELECT id, 'building' as "type", 'red' as color, ST_AsX3D((p.geom), 4.0, 0) as geom FROM polygons p;
