82b12c453634b5dfe757a9d19f04818f40810050
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-04-28 18:06:41 +0200

    Improve the script to use pre-defined paths

--- apps/bgt/scripts/gml2csv.sh
+++ apps/bgt/scripts/gml2csv.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+LOG=/tmp/mylog
 
-for f in `ls extract`; do d=${f%.*}; echo ${d}; ogr2ogr -f csv -lco GEOEMTRY=AS_WKT outCsv/$d.csv extract/$d.gml ; done
+##################
+function if_error
+##################
+{
+if [[ $? -ne 0 ]]; then # check return code passed to function
+print "$1 TIME:$TIME" | tee -a $LOG # if rc > 0 then print error msg and quit
+exit $?
+fi
+}
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+echo ""
+echo "Reading monetdb config..." >&2
+. $DIR/../../../configs/monetdb.cfg
+
+scripts=$monetdb_scripts
+echo "MonetDB scripts are located at: $scripts" >&2
+echo ""
+
+echo "Reading bgt config..." >&2
+. $DIR/../../../configs/bgt.cfg
+
+data_path=$bgt_data_path
+echo "Output data will be stored at: $data_path" >&2
+echo ""
+
+extract_path=$bgt_extract_path
+echo "Input data is located at: $data_path" >&2
+echo ""
+
+for f in `ls $extract_path`; do d=${f%.*}; echo ${d}; ogr2ogr -f csv -lco GEOEMTRY=AS_WKT $data_path/$d.csv $extract_path/$d.gml ; done
 
 #Sed commands in case we want to remove double cotes and use | as the atttribute seperator
 #sed -i 's/",\(\w\)/"\nclean,\1/g' *.csv
