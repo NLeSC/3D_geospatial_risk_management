00151d0f1076fd747c6c1fff7c923602ded0abb5
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-05-20 11:34:57 +0200

    Extend the area. Use contains instead of within, no need to run IsValid

--- apps/x3d_viewer/queries/bgt_buildings.sql
+++ apps/x3d_viewer/queries/bgt_buildings.sql
@@ -2,24 +2,23 @@ declare _west integer;
 declare _south integer;
 declare _east integer;
 declare _north integer;
-set _west = 93816;
-set _east = 93916;
-set _south = 463891;
-set _north = 463991;
+set _west = 93816.0;
+set _east = 93916.0;
+set _south = 463891.0;
+set _north = 463991.0;
 
 WITH 
 bounds AS (
-    SELECT ST_SetSRID(ST_MakeEnvelope(_west, _south, _east, _north, 28992), 4326) as geom 
+    SELECT ST_MakeEnvelope(_west, _south, _east, _north, 28992) as geom 
 ), 
 pointcloud AS (
 	SELECT x, y, z 
 	FROM ahn3, bounds 
 	WHERE 
-    --x between _west and _east and
-    --y between _south and _north and
-    x between 93816 and 93916 and
-    y between 463891 and 463991 and
-	ST_DWithin(geom, ST_SetSRID(ST_MakePoint(x, y, z), 4326), 10)
+    x between 93816.0 and 93916.0 and
+    y between 463891.0 and 463991.0 and
+	--ST_DWithin(geom, ST_SetSRID(ST_MakePoint(x, y, z), 4326), 10)
+	Contains(geom, x, y)
 	and c = 6
 ),
 footprints AS (
@@ -30,9 +29,9 @@ footprints AS (
 	FROM bgt_buildingpart a, bounds b
 	WHERE 1 = 1
 	AND ST_Area(a.wkt) > 30
-	AND ST_Intersects(ST_SetSRID(a.wkt, 4326), b.geom)
-	AND ST_Intersects(ST_SetSRID(ST_Centroid(a.wkt), 4326), b.geom)
-	AND ST_IsValid(a.wkt)
+	AND ST_Intersects(a.wkt, b.geom)
+	AND ST_Intersects(ST_Centroid(a.wkt), b.geom)
+	--AND ST_IsValid(a.wkt)
 ),
 --papoints AS ( --get points from intersecting patches
 --	SELECT 
