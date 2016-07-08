fdcdfa32d9ae387b0e16c42b906f4cb1175023e9
Tom van Tilburg <tomt.van.tilburg@gmail.com>
2016-07-08 19:15:09 +0200

    pg2monetdb ready for polygons

--- apps/bgt/scripts/pg2monetdb/bgt2csv.sql
+++ apps/bgt/scripts/pg2monetdb/bgt2csv.sql
@@ -1,6 +1,8 @@
 /*
-Getting the BGT out of postgis into csv files
+	Getting the BGT out of postgis into csv files
+	(only polygons so far...)
 */
+
 COPY (SELECT ogc_fid,ST_AsText(wkb_geometry) wkt,namespace,lokaalid,objectbegintijd,objecteindtijd,tijdstipregistratie,eindregistratie,lv_publicatiedatum,bronhouder,CASE WHEN inonderzoek = true THEN 'true' ELSE 'false' END AS inonderzoek,relatievehoogteligging,bgt_status,plus_status,bgt_type,plus_type 
 FROM bgt_import2.waterdeel_2d) TO '/tmp/waterdeel_2d.csv'  WITH DELIMITER '|' CSV HEADER;
 
@@ -12,3 +14,25 @@ FROM bgt_import2.begroeidterreindeel_2d) TO '/tmp/begroeidterreindeel_2d.csv'  W
 
 COPY (SELECT ogc_fid,ST_AsText(wkb_geometry) wkt,namespace,lokaalid,objectbegintijd,objecteindtijd,tijdstipregistratie,eindregistratie,lv_publicatiedatum,bronhouder,CASE WHEN inonderzoek = true THEN 'true' ELSE 'false' END AS inonderzoek,relatievehoogteligging,bgt_status,plus_status, identificatiebagpnd 
 FROM bgt_import2.pand_2d) TO '/tmp/pand_2d.csv'  WITH DELIMITER '|' CSV HEADER;
+
+COPY (SELECT ogc_fid,ST_AsText(wkb_geometry) wkt,namespace,lokaalid,objectbegintijd,objecteindtijd,tijdstipregistratie,eindregistratie,lv_publicatiedatum,bronhouder,CASE WHEN inonderzoek = true THEN 'true' ELSE 'false' END AS inonderzoek,relatievehoogteligging,bgt_status,plus_status, bgt_type, plus_type 
+FROM bgt_import2.ondersteunendwaterdeel_2d) TO '/tmp/ondersteunendwaterdeel_2d.csv'  WITH DELIMITER '|' CSV HEADER;
+
+COPY (SELECT ogc_fid,ST_AsText(wkb_geometry) wkt,namespace,lokaalid,objectbegintijd,objecteindtijd,tijdstipregistratie,eindregistratie,lv_publicatiedatum,bronhouder,CASE WHEN inonderzoek = true THEN 'true' ELSE 'false' END AS inonderzoek,relatievehoogteligging,bgt_status,plus_status, bgt_functie, plus_functie, bgt_fysiekvoorkomen, plus_fysiekvoorkomen,CASE WHEN ondersteunendwegdeeloptalud  = true THEN 'true' ELSE 'false' END AS ondersteunendwegdeeloptalud 
+FROM bgt_import2.ondersteunendwegdeel_2d) TO '/tmp/ondersteunendwegdeel_2d.csv'  WITH DELIMITER '|' CSV HEADER;
+
+COPY (SELECT ogc_fid,ST_AsText(wkb_geometry) wkt,namespace,lokaalid,objectbegintijd,objecteindtijd,tijdstipregistratie,eindregistratie,lv_publicatiedatum,bronhouder,CASE WHEN inonderzoek = true THEN 'true' ELSE 'false' END AS inonderzoek,relatievehoogteligging,bgt_status,plus_status, typeoverbruggingsdeel, hoortbijtypeoverbrugging, CASE WHEN overbruggingisbeweegbaar = true THEN 'true' ELSE 'false' END AS overbruggingisbeweegbaar  
+FROM bgt_import2.overbruggingsdeel_2d) TO '/tmp/overbruggingsdeel_2d.csv'  WITH DELIMITER '|' CSV HEADER;
+
+COPY (SELECT ogc_fid,ST_AsText(wkb_geometry) wkt,namespace,lokaalid,objectbegintijd,objecteindtijd,tijdstipregistratie,eindregistratie,lv_publicatiedatum,bronhouder,CASE WHEN inonderzoek = true THEN 'true' ELSE 'false' END AS inonderzoek,relatievehoogteligging,bgt_status,plus_status, bgt_type, plus_type 
+FROM bgt_import2.overigbouwwerk_2d) TO '/tmp/overigbouwwerk_2d.csv'  WITH DELIMITER '|' CSV HEADER;
+
+COPY (SELECT ogc_fid,ST_AsText(wkb_geometry) wkt,namespace,lokaalid,objectbegintijd,objecteindtijd,tijdstipregistratie,eindregistratie,lv_publicatiedatum,bronhouder,CASE WHEN inonderzoek = true THEN 'true' ELSE 'false' END AS inonderzoek,relatievehoogteligging,bgt_status,plus_status, bgt_type, plus_type 
+FROM bgt_import2.scheiding_2d) TO '/tmp/scheiding_2d.csv'  WITH DELIMITER '|' CSV HEADER;
+
+COPY (SELECT ogc_fid,ST_AsText(wkb_geometry) wkt,namespace,lokaalid,objectbegintijd,objecteindtijd,tijdstipregistratie,eindregistratie,lv_publicatiedatum,bronhouder,CASE WHEN inonderzoek = true THEN 'true' ELSE 'false' END AS inonderzoek,relatievehoogteligging,bgt_status,plus_status 
+FROM bgt_import2.tunneldeel_2d) TO '/tmp/tunneldeel_2d.csv'  WITH DELIMITER '|' CSV HEADER;
+
+/* Still a problem with wegdeel since it has curvey geometries */
+COPY (SELECT ogc_fid,ST_AsText(wkb_geometry) wkt,namespace,lokaalid,objectbegintijd,objecteindtijd,tijdstipregistratie,eindregistratie,lv_publicatiedatum,bronhouder,CASE WHEN inonderzoek = true THEN 'true' ELSE 'false' END AS inonderzoek,relatievehoogteligging,bgt_status,plus_status,bgt_fysiekvoorkomen,plus_fysiekvoorkomen, CASE WHEN wegdeeloptalud  = true THEN 'true' ELSE 'false' END AS wegdeeloptalud 
+FROM bgt_import2.wegdeel_2d) TO '/tmp/wegdeel_2d.csv'  WITH DELIMITER '|' CSV HEADER;
--- apps/bgt/scripts/pg2monetdb/create_tables.sql
+++ apps/bgt/scripts/pg2monetdb/create_tables.sql
@@ -1,5 +1,6 @@
 /* 
 	Create tables in monetdb, according to NLExtract setup for postgis
+	(only polygons so far...)
 */
 
 create table bgt_waterdeel_2d (
@@ -76,4 +77,139 @@ create table bgt_pand_2d (
         bgt_status string,
         plus_status string,
         identificatiebagpnd string
+);
+create table bgt_ondersteunendwaterdeel_2d (
+        ogc_fid string,
+        wkt geometry,
+        namespace string,
+        lokaalid string,
+        objectbegintijd timestamp,
+        objecteindtijd timestamp,
+        tijdstipregistratie timestamp,
+        eindregistratie timestamp,
+        lv_publicatiedatum timestamp,
+        bronhouder string,
+        inonderzoek boolean,
+        relatievehoogteligging integer,
+        bgt_status string,
+        plus_status string,
+        bgt_type string,
+        plus_type string
+);
+
+create table bgt_ondersteunendwegdeel_2d (
+        ogc_fid string,
+        wkt geometry,
+        namespace string,
+        lokaalid string,
+        objectbegintijd timestamp,
+        objecteindtijd timestamp,
+        tijdstipregistratie timestamp,
+        eindregistratie timestamp,
+        lv_publicatiedatum timestamp,
+        bronhouder string,
+        inonderzoek boolean,
+        relatievehoogteligging integer,
+        bgt_status string,
+        plus_status string,
+        bgt_fysiekvoorkomen string,
+        plus_fysiekvoorkomen string,
+        ondersteunendwegdeeloptalud boolean
+);
+
+create table bgt_overbruggingsdeel_2d (
+        ogc_fid string,
+        wkt geometry,
+        namespace string,
+        lokaalid string,
+        objectbegintijd timestamp,
+        objecteindtijd timestamp,
+        tijdstipregistratie timestamp,
+        eindregistratie timestamp,
+        lv_publicatiedatum timestamp,
+        bronhouder string,
+        inonderzoek boolean,
+        relatievehoogteligging integer,
+        bgt_status string,
+        plus_status string,
+        ypeoverbruggingsdeel string, 
+        hoortbijtypeoverbrugging string, 
+        ondersteunendwegdeeloptalud boolean
+);
+
+
+create table bgt_overigbouwwerk_2d (
+        ogc_fid string,
+        wkt geometry,
+        namespace string,
+        lokaalid string,
+        objectbegintijd timestamp,
+        objecteindtijd timestamp,
+        tijdstipregistratie timestamp,
+        eindregistratie timestamp,
+        lv_publicatiedatum timestamp,
+        bronhouder string,
+        inonderzoek boolean,
+        relatievehoogteligging integer,
+        bgt_status string,
+        plus_status string,
+        bgt_type string,
+        plus_type string
+);
+
+create table bgt_scheiding_2d (
+        ogc_fid string,
+        wkt geometry,
+        namespace string,
+        lokaalid string,
+        objectbegintijd timestamp,
+        objecteindtijd timestamp,
+        tijdstipregistratie timestamp,
+        eindregistratie timestamp,
+        lv_publicatiedatum timestamp,
+        bronhouder string,
+        inonderzoek boolean,
+        relatievehoogteligging integer,
+        bgt_status string,
+        plus_status string,
+        bgt_type string,
+        plus_type string
+);
+
+create table bgt_tunneldeel_2d (
+        ogc_fid string,
+        wkt geometry,
+        namespace string,
+        lokaalid string,
+        objectbegintijd timestamp,
+        objecteindtijd timestamp,
+        tijdstipregistratie timestamp,
+        eindregistratie timestamp,
+        lv_publicatiedatum timestamp,
+        bronhouder string,
+        inonderzoek boolean,
+        relatievehoogteligging integer,
+        bgt_status string,
+        plus_status string
+  
+);
+
+create table bgt_wegdeel_2d (
+        ogc_fid string,
+        wkt geometry,
+        namespace string,
+        lokaalid string,
+        objectbegintijd timestamp,
+        objecteindtijd timestamp,
+        tijdstipregistratie timestamp,
+        eindregistratie timestamp,
+        lv_publicatiedatum timestamp,
+        bronhouder string,
+        inonderzoek boolean,
+        relatievehoogteligging integer,
+        bgt_status string,
+        plus_status string,
+        bgt_fysiekvoorkomen string,
+        plus_fysiekvoorkomen string,
+        wegdeeloptalud boolean
 );
