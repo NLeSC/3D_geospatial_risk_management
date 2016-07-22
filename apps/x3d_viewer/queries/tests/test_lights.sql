4e775e4ae656fe579b01639945ed8697d6daaac6
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-07-22 13:03:16 +0200

    Filter joins

--- apps/x3d_viewer/queries/tests/test_lights.sql
+++ apps/x3d_viewer/queries/tests/test_lights.sql
@@ -21,11 +21,10 @@ create table pointcloud_unclassified AS(
 		ST_SetSRID(ST_MakePoint(x, y, z), 28992) as geom, z
 	FROM C_30FZ1, bounds 
 	WHERE 
-        --ST_DWithin(geom, Geometry(pa),10) --patches should be INSIDE bounds
         x between 93816.0 and 93916.0 and
         y between 463891.0 and 463991.0 and
-        --ST_Intersects(geom, Geometry(pa))
-    	Contains(geom, x, y) and
+        --ST_DWithin(geom, Geometry(pa),10) --patches should be INSIDE bounds
+        [geom] DWithin [x, y, z, 28992, 10] and--patches should be INSIDE bounds
         c = 1 and
         r = 1 and
         i > 150
@@ -37,7 +36,7 @@ create table points AS (
 	FROM bgt_paal a, bounds b 
 	WHERE 
     (plus_type = 'lichtmast' OR plus_type Is Null)
-	AND ST_Intersects(a.wkt, b.geom)
+	AND [a.wkt] Intersects [b.geom]
 ) WITH DATA;
 
 drop table pointsz;
@@ -46,7 +45,8 @@ create table pointsz AS (
 	FROM points a
 	LEFT JOIN 
     pointcloud_unclassified b ON
-    ST_DWithin(b.geom, a.geom,1)
+    --ST_DWithin(b.geom, a.geom,1)
+    [b.beom] DWithin [a.geom,1]
 	GROUP BY a.id, a.geom
 ) WITH DATA;
 
