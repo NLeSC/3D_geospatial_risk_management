889f6c801f6afda6471503786d0485333d534452
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-07-22 14:02:10 +0200

    Filter joins

--- apps/x3d_viewer/queries/bgt_treepoints.sql
+++ apps/x3d_viewer/queries/bgt_treepoints.sql
@@ -24,9 +24,9 @@ pointcloud_unclassified AS(
         C_30FZ1, bounds
 	WHERE
     --ST_DWithin(geom, Geometry(pa),10) --patches should be INSIDE bounds
+    [geom] DWithin [x, y, z, 28992, 10] and --patches should be INSIDE bounds
     x between 93816.0 and 93916.0 and
     y between 463891.0 and 463991.0 and
-	Contains(geom, x, y) and
     c = 1 AND
     i < 150 AND
     r < n-1
