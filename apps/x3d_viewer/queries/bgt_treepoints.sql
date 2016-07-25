c9c2143a00835d0beb5a4bd27b4b231e8a0f35b4
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-07-25 12:00:41 +0200

    Use a contains instead of DWithin because of a query plans optimization issue.

--- apps/x3d_viewer/queries/bgt_treepoints.sql
+++ apps/x3d_viewer/queries/bgt_treepoints.sql
@@ -15,7 +15,8 @@ CREATE SEQUENCE "counter" AS INTEGER;
 
 WITH
 bounds AS (
-	SELECT ST_MakeEnvelope(_west, _south, _east, _north, 28992) as geom
+	--SELECT ST_MakeEnvelope(_west, _south, _east, _north, 28992) as geom
+	SELECT ST_MakeEnvelope(_west+10, _south+10, _east+10, _north+10, 28992) as geom
 ),
 pointcloud_unclassified AS(
 	SELECT
@@ -24,11 +25,12 @@ pointcloud_unclassified AS(
         C_30FZ1, bounds
 	WHERE
     --ST_DWithin(geom, Geometry(pa),10) --patches should be INSIDE bounds
-    [geom] DWithin [x, y, z, 28992, 10] and --patches should be INSIDE bounds
     x between 93816.0 and 93916.0 and
     y between 463891.0 and 463991.0 and
     c = 1 AND
     i < 150 AND
-    r < n-1
+    r < n-1 and
+    --[geom] DWithin [x, y, z, 28992, 10] --patches should be INSIDE bounds
+    Contains(geom, x, y) --patches should be INSIDE bounds
 )
 SELECT NEXT VALUE for "counter" as id, 'tree' as type, '0 ' || rand() * 0.1 ||' 0' as color, ST_AsX3D(ST_Collect(geom), 4.0, 0) as geom FROM pointcloud_unclassified a;
