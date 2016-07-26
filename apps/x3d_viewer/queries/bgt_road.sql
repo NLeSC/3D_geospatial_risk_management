0a75bfb53251a2a9337d3a6ce7c94751f8e2bd2d
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-07-26 17:13:14 +0200

    It is now supported since we do not need patch_to_geom

--- apps/x3d_viewer/queries/bgt_road.sql
+++ apps/x3d_viewer/queries/bgt_road.sql
@@ -13,87 +13,123 @@ set _segmentlength = 10;
 
 WITH
 bounds AS (
-	SELECT ST_Segmentize(ST_MakeEnvelope(_west, _south, _east, _north, 28992),_segmentlength) geom
+	SELECT ST_Segmentize(ST_MakeEnvelope(_west, _south, _east, _north, 28992),_segmentlength) as geom
 ),
-
+bgt_wegdeel_light AS (
+	--SELECT a.ogc_fid, 'road' AS class, a.bgt_functie as type, ST_Intersection(a.wkt,c.geom) as geom 
+	SELECT a.ogc_fid, 'road' AS class, a.bgt_status as type, a.wkt as wkt, ST_Intersection(a.wkt,c.geom) as geom 
+	FROM bgt_wegdeel a, bounds c
+	WHERE 
+    a.relatieveHoogteligging = 0 AND
+	a.eindregistratie Is Null AND
+	[geom] Intersects [a.wkt]
+),
 mainroads AS (
-	SELECT a.ogc_fid, 'road'::text AS class, a.bgt_functie as type, ST_Intersection(a.wkb_geometry,c.geom) geom
-	FROM bgt_import2.wegdeel_2d a
-	LEFT JOIN bgt_import2.overbruggingsdeel_2d b
-	ON ([a.wkt] Intersects [b.wkt]) AND St_Contains(ST_buffer((b.wkb_geometry),1), (a.wkb_geometry)))
-	,bounds c
-	WHERE a.relatieveHoogteligging = 0
-	AND ST_CurveToLine(b.wkb_geometry) Is Null
-	AND a.eindregistratie Is Null
-	AND b.eindregistratie Is Null
-	AND [geom] Intersects [a.wkb_geometry]
+	SELECT a.ogc_fid, a.class, a.type, a.geom 
+	FROM bgt_wegdeel_light a
+	LEFT JOIN bgt_overbruggingsdeel b
+	ON ([a.wkt] Intersects [b.wkt]) AND [ST_buffer((b.wkt),1)] Contains [a.wkt]
+	WHERE 
+	--AND ST_CurveToLine(b.wkt) Is Null
+	b.eindregistratie Is Null
+	AND [geom] Intersects [a.wkt]
 ),
 auxroads AS (
-	SELECT ogc_fid, 'road'::text AS class, bgt_functie as type, ST_Intersection(wkb_geometry,geom) geom
-	FROM bgt_import2.ondersteunendwegdeel_2d, bounds
+	SELECT ogc_fid, 'road' AS class, bgt_functie as type, ST_Intersection(wkt,geom) as geom
+	FROM bgt_ondersteunendwegdeel, bounds
 	WHERE relatieveHoogteligging = 0
 	AND eindregistratie Is Null
-	AND [geom] Intersects [wkb_geometry]
+	--AND ST_Intersects(geom, wkb_geometry)
+	AND [geom] Intersects [wkt]
 ),
 tunnels AS (
-	SELECT ogc_fid, 'road'::text AS class, 'tunnel'::text as type, ST_Intersection(wkb_geometry,geom) geom
-	FROM bgt_import2.tunneldeel_2d, bounds
+	SELECT ogc_fid, 'road' AS class, 'tunnel' as type, ST_Intersection(wkt,geom) as geom
+	FROM bgt_tunneldeel, bounds
 	WHERE eindregistratie Is Null
-	AND [geom] Intersects [wkb_geometry]
+	AND [geom] Intersects [wkt]
 ),
 pointcloud_ground AS (
-	SELECT PC_FilterEquals(pa,'classification',2) pa
-	FROM ahn3_pointcloud.vw_ahn3, bounds
-	WHERE ST_Intersects(geom, Geometry(pa))
-	--Sometimes roads are incorrectly classified as bridges, so also include bridge points
-	/*UNION ALL
-	SELECT PC_FilterEquals(pa,'classification',26) pa
-	FROM ahn3_pointcloud.vw_ahn3, bounds
-	WHERE ST_Intersects(geom, Geometry(pa))*/
+	SELECT x, y, z
+	--FROM ahn3, bounds
+	FROM C_30FZ1, bounds
+	WHERE 
+    --ST_Intersects(geom, x, y, z, 28992) AND
+    --[geom] Intersects [x, y, z, 28992] AND
+    c = 2 and
+    x between 93816.0 and 93916.0 and
+    y between 463891.0 and 463991.0 and
+    Contains(geom, x, y)
 ),
-polygons AS (
-	SELECT nextval('counter') id, ogc_fid fid, type, class,(ST_Dump(geom)).geom
+polygons_b AS (
+	SELECT ogc_fid as fid, type, class, geom
 	FROM mainroads
 	UNION ALL
-	SELECT nextval('counter') id, ogc_fid fid, type, class,(ST_Dump(geom)).geom
+	SELECT ogc_fid as fid, type, class, geom
 	FROM auxroads
 	UNION ALL
-	SELECT nextval('counter') id, ogc_fid fid, type, class,(ST_Dump(geom)).geom
+	SELECT ogc_fid as fid, type, class, geom
 	FROM tunnels
-)
-,polygonsz AS (
-	SELECT id, fid, type, class, patch_to_geom(PC_Union(b.pa), geom) geom
+),
+polygons_dump AS (
+    SELECT parent as fid, polygonWKB as geom
+    FROM ST_DUMP((select geom, fid from polygons)) d
+),
+polygons AS (
+   select a.fid, a.type, a.class, b.geom
+   FROM
+   polygons_b a LEFT JOIN polygons_dump b
+   ON a.fid = b.fid
+),
+polygonsz AS (
+	SELECT fid, type, class, geom
 	FROM polygons a
 	LEFT JOIN pointcloud_ground b
-	ON ST_Intersects(geom, x, y, z, 28992)
-	GROUP BY id, fid, type, class, geom
-)
-,basepoints AS (
-	SELECT id,geom FROM polygonsz
+	--ON ST_Intersects(geom,Geometry(b.pa))
+	ON [geom] Intersects [x, y, z, 28992]
+	WHERE 
+        ST_IsValid(geom)
+	GROUP BY fid, type, class, geom
+),
+edge_points AS (
+    --SELECT parent as polygon_id, cast(path as int) as path, ST_SetSRID(pointg, 28992) as geom FROM ST_DumpPoints((select geom, fid from polygonsz)) d
+    SELECT parent as polygon_id, cast((SUBSTRING(path, POSITION(',' IN path)+1)) as int) as path, ST_SetSRID(pointg, 28992) as geom FROM ST_DumpPoints((select geom, fid from polygonsz)) d
+),
+emptyz AS (
+    --SELECT polygon_id, a.path as path, a.geom as geom , b.z as z, ST_Distance(ST_SetSRID(a.geom, 28992), ST_SetSRID(ST_MakePoint(x, y, z), 28992)) as dist FROM edge_points a, pointcloud_ground b WHERE ST_DWithin(a.geom, x, y, z, 28992, 10)
+    --SELECT polygon_id, a.path as path, a.geom as geom , b.z as z, ST_Distance(a.geom, x, y, z, 28992) as dist FROM edge_points a, pointcloud_ground b WHERE ST_DWithin(a.geom, x, y, z, 28992, 10)
+    SELECT polygon_id, a.path as path, a.geom as geom , b.z as z, ST_Distance(a.geom, x, y, z, 28992) as dist FROM edge_points a, pointcloud_ground b WHERE [a.geom] DWithin [x, y, z, 28992, 10]
+),
+ranktest AS (
+    select polygon_id, path, geom, z, dist, RANK() over (PARTITION BY path, geom order by polygon_id, path, dist ASC) as rank from emptyz
+    --select polygon_id, path, geom, z, dist, RANK() over (PARTITION BY polygon_id, path, geom) as rank from emptyz
+),
+filledz AS (
+    --select polygon_id, path, ST_MakePoint(ST_X(geom), ST_Y(geom), z) as geom from ranktest where rank = 1 order by path
+    select polygon_id, path, ST_MakePoint(ST_X(geom), ST_Y(geom), z) as geom from ranktest where rank = 1
+),
+line_z AS (
+    SELECT polygon_id, ST_MakeLine(geom) as geom FROM filledz group by polygon_id
+),
+basepoints AS (
+	SELECT polygon_id as id, ST_Triangulate2DZ(ST_Collect(geom),0) as geom FROM line_z
 	WHERE ST_IsValid(geom)
-)
-,triangles AS (
-	SELECT
-		id,
-		ST_MakePolygon(
-			ST_ExteriorRing(
-				(ST_Dump(ST_Triangulate2DZ(ST_Collect(a.geom)))).geom
-			)
-		)geom
-	FROM basepoints a
-	GROUP BY id
-)
-,assign_triags AS (
+    GROUP BY id
+),
+triangles AS (
+    SELECT parent as id, ST_SetSRID(ST_MakePolygon(ST_ExteriorRing( a.polygonWKB)), 28992) as geom FROM ST_Dump((select geom, id from basepoints)) a
+),
+assign_triags AS (
 	SELECT 	a.*, b.type, b.class
 	FROM triangles a
-	INNER JOIN polygons b
-	ON ST_Contains(b.geom, a.geom)
-	,bounds c
-	WHERE [ST_Centroid(b.geom)] Intersects [c.geom]
-	AND a.id = b.id
+	--INNER JOIN polygons b
+	, polygons b
+	, bounds c
+	WHERE 
+    --ST_Intersects(ST_Centroid(b.geom), c.geom) AND
+    [ST_Centroid(b.geom)] Intersects [c.geom] AND
+	a.id = b.fid AND
+	--ON ST_Contains(b.geom, a.geom)
+	[b.geom] Contains [a.geom]
 )
 
-SELECT _south::text || _west::text || p.id as id, p.type as type,
-	ST_AsX3D(ST_Collect(p.geom),5) geom
-FROM assign_triags p
-GROUP BY p.id, p.type
+SELECT p.id as id, p.type as type, ST_AsX3D(ST_Collect(p.geom),5.0, 0) as geom FROM assign_triags p GROUP BY p.id, p.type;
--- apps/x3d_viewer/queries/tests/test_road.sql
+++ apps/x3d_viewer/queries/tests/test_road.sql
@@ -10,44 +10,50 @@ set _south = 463891.0;
 set _north = 463991.0;
 set _segmentlength = 10;
 
-
 drop table bounds;
 create table bounds AS (
 	SELECT ST_Segmentize(ST_MakeEnvelope(_west, _south, _east, _north, 28992),_segmentlength) as geom
 ) WITH DATA;
 
+drop table bgt_wegdeel_light;
+create table bgt_wegdeel_light AS (
+	--SELECT a.ogc_fid, 'road' AS class, a.bgt_functie as type, ST_Intersection(a.wkt,c.geom) as geom 
+	SELECT a.ogc_fid, 'road' AS class, a.bgt_status as type, a.wkt as wkt, ST_Intersection(a.wkt,c.geom) as geom 
+	FROM bgt_wegdeel a, bounds c
+	WHERE 
+    a.relatieveHoogteligging = 0 AND
+	a.eindregistratie Is Null AND
+	[geom] Intersects [a.wkt]
+) WITH DATA;
+
 drop table mainroads;
 create table mainroads AS (
-	SELECT a.ogc_fid, 'road' AS class, a.bgt_functie as type, ST_Intersection(a.wkt,c.geom) as geom 
-	FROM bgt_wegdeel a
+	SELECT a.ogc_fid, a.class, a.type, a.geom 
+	FROM bgt_wegdeel_light a
 	LEFT JOIN bgt_overbruggingsdeel b
-	--ON (St_Intersects((a.wkt), (b.wkt)) AND St_Contains(ST_buffer((b.wkt),1), (a.wkt)))
-	ON ([a.wkt] Intersects [b.wkt]) AND St_Contains(ST_buffer((b.wkt),1), (a.wkt))
-	, bounds c
-	WHERE a.relatieveHoogteligging = 0
-	AND ST_CurveToLine(b.wkt) Is Null
-	AND a.eindregistratie Is Null
-	AND b.eindregistratie Is Null
-	--AND ST_Intersects(geom, a.wkb_geometry)
+	ON ([a.wkt] Intersects [b.wkt]) AND [ST_buffer((b.wkt),1)] Contains [a.wkt]
+	WHERE 
+	--AND ST_CurveToLine(b.wkt) Is Null
+	b.eindregistratie Is Null
 	AND [geom] Intersects [a.wkt]
 ) WITH DATA;
 
 drop table auxroads;
 create table auxroads AS (
-	SELECT ogc_fid, 'road' AS class, bgt_functie as type, ST_Intersection(wkb_geometry,geom) as geom
+	SELECT ogc_fid, 'road' AS class, bgt_functie as type, ST_Intersection(wkt,geom) as geom
 	FROM bgt_ondersteunendwegdeel, bounds
 	WHERE relatieveHoogteligging = 0
 	AND eindregistratie Is Null
 	--AND ST_Intersects(geom, wkb_geometry)
-	AND [geom] Intersects [wkb_geometry]
+	AND [geom] Intersects [wkt]
 ) WITH DATA;
 
 drop table tunnels;
 create table tunnels AS (
-	SELECT ogc_fid, 'road' AS class, 'tunnel' as type, ST_Intersection(wkb_geometry,geom) as geom
+	SELECT ogc_fid, 'road' AS class, 'tunnel' as type, ST_Intersection(wkt,geom) as geom
 	FROM bgt_tunneldeel, bounds
 	WHERE eindregistratie Is Null
-	AND [geom] Intersects [wkb_geometry]
+	AND [geom] Intersects [wkt]
 ) WITH DATA;
 
 drop table pointcloud_ground;
@@ -56,65 +62,95 @@ create table pointcloud_ground AS (
 	FROM ahn3, bounds
 	WHERE 
     --ST_Intersects(geom, x, y, z, 28992) AND
-    [geom] Intersects [x, y, z, 28992] AND
+    --[geom] Intersects [x, y, z, 28992] AND
+    x between _west and _east and
+    y between _south and _north and 
+    Contains(geom, x, y) AND
     c = 2
 ) WITH DATA;
 
-DROP SEQUENCE "counter";
-CREATE SEQUENCE "counter" AS INTEGER;
-
-drop table polygons;
-create table polygons AS (
-	SELECT next value for "counter" as id, ogc_fid as fid, type, class, geom
+drop table polygons_b;
+create table polygons_b AS (
+	SELECT ogc_fid as fid, type, class, geom
 	FROM mainroads
 	UNION ALL
-	SELECT next value for "counter" as id, ogc_fid as fid, type, class, geom
+	SELECT ogc_fid as fid, type, class, geom
 	FROM auxroads
 	UNION ALL
-	SELECT next value for "counter" as id, ogc_fid as fid, type, class, geom
+	SELECT ogc_fid as fid, type, class, geom
 	FROM tunnels
 ) WITH DATA;
 
 drop table polygons_dump;
 create table polygons_dump AS (
-    SELECT parent as id, polygonWKB as geom
-    FROM ST_DUMP((select geom, id from polygons)) d
+    SELECT parent as fid, polygonWKB as geom
+    FROM ST_DUMP((select geom, fid from polygons)) d
+) WITH DATA;
+
+drop table polygons;
+create table polygons AS (
+   select a.fid, a.type, a.class, b.geom
+   FROM
+   polygons_b a LEFT JOIN polygons_dump b
+   ON a.fid = b.fid
 ) WITH DATA;
 
 drop table polygonsz;
 create table polygonsz AS (
-	SELECT id, fid, type, class, patch_to_geom(x, y, z, geom) as geom
-	FROM polygons a,
+	SELECT fid, type, class, geom
+	FROM polygons a
 	LEFT JOIN pointcloud_ground b
 	--ON ST_Intersects(geom,Geometry(b.pa))
 	ON [geom] Intersects [x, y, z, 28992]
-    , polygons_dump c
 	WHERE 
-        a.id = c.id AND
         ST_IsValid(geom)
-	GROUP BY id, fid, type, class, geom
+	GROUP BY fid, type, class, geom
+) WITH DATA;
+
+drop table edge_points;
+create table edge_points AS (
+    --SELECT parent as polygon_id, cast(path as int) as path, ST_SetSRID(pointg, 28992) as geom FROM ST_DumpPoints((select geom, fid from polygonsz)) d
+    SELECT parent as polygon_id, cast((SUBSTRING(path, POSITION(',' IN path)+1)) as int) as path, ST_SetSRID(pointg, 28992) as geom FROM ST_DumpPoints((select geom, fid from polygonsz)) d
+) WITH DATA;
+--insert into _emptyz SELECT a.path as path, a.geom as geom , b.z as z, ST_Distance(ST_SetSRID(a.geom, 28992), ST_SetSRID(ST_MakePoint(x, y, z), 28992)) as dist FROM _edge_points a, pointcloud_ground b;
+
+drop table emptyz;
+create table emptyz AS (
+    --SELECT polygon_id, a.path as path, a.geom as geom , b.z as z, ST_Distance(ST_SetSRID(a.geom, 28992), ST_SetSRID(ST_MakePoint(x, y, z), 28992)) as dist FROM edge_points a, pointcloud_ground b WHERE ST_DWithin(a.geom, x, y, z, 28992, 10)
+    --SELECT polygon_id, a.path as path, a.geom as geom , b.z as z, ST_Distance(a.geom, x, y, z, 28992) as dist FROM edge_points a, pointcloud_ground b WHERE ST_DWithin(a.geom, x, y, z, 28992, 10)
+    SELECT polygon_id, a.path as path, a.geom as geom , b.z as z, ST_Distance(a.geom, x, y, z, 28992) as dist FROM edge_points a, pointcloud_ground b WHERE [a.geom] DWithin [x, y, z, 28992, 10]
+) WITH DATA;
+--insert into _ranktest select path, geom, z, dist, RANK() over (PARTITION BY path, geom order by path, dist ASC) as rank from _emptyz;
+
+drop table ranktest;
+create table ranktest AS (
+    select polygon_id, path, geom, z, dist, RANK() over (PARTITION BY path, geom order by polygon_id, path, dist ASC) as rank from emptyz
+    --select polygon_id, path, geom, z, dist, RANK() over (PARTITION BY polygon_id, path, geom) as rank from emptyz
+) WITH DATA;
+--insert into _filledz select path, ST_MakePoint(ST_X(geom), ST_Y(geom), z) as geom from _ranktest where rank = 1 order by path;
+
+drop table filledz;
+create table filledz AS (
+    --select polygon_id, path, ST_MakePoint(ST_X(geom), ST_Y(geom), z) as geom from ranktest where rank = 1 order by path
+    select polygon_id, path, ST_MakePoint(ST_X(geom), ST_Y(geom), z) as geom from ranktest where rank = 1
+) WITH DATA;
+--insert into _line_z SELECT ST_MakeLine(geom) as geom FROM _filledz;
+
+drop table line_z;
+create table line_z AS (
+    SELECT polygon_id, ST_MakeLine(geom) as geom FROM filledz group by polygon_id
 ) WITH DATA;
 
 drop table basepoints;
 create table basepoints AS (
-	SELECT id, ST_Triangulate2DZ(ST_Collect(geom)) FROM polygonsz
+	SELECT polygon_id as id, ST_Triangulate2DZ(ST_Collect(geom),0) as geom FROM line_z
 	WHERE ST_IsValid(geom)
     GROUP BY id
 ) WITH DATA;
 
-drop table basepoints_dump;
-create table basepoints_dump AS (
-    select parent as id, polygonWKB as geom
-    FROM ST_DUMP((select geom, id from basepoints)) d
-) WITH DATA;
-
 drop table triangles;
 create table triangles AS (
-	SELECT  id,
-		ST_MakePolygon(ST_ExteriorRing(a.geom)) as geom
-	FROM basepoints a, basepoints_dump b
-    WHERE
-    a.id = b.id;
+    SELECT parent as id, ST_SetSRID(ST_MakePolygon(ST_ExteriorRing( a.polygonWKB)), 28992) as geom FROM ST_Dump((select geom, id from basepoints)) a
 ) WITH DATA;
 
 drop table assign_triags;
@@ -127,9 +163,9 @@ create table assign_triags AS (
 	WHERE 
     --ST_Intersects(ST_Centroid(b.geom), c.geom) AND
     [ST_Centroid(b.geom)] Intersects [c.geom] AND
-	a.id = b.id AND
+	a.id = b.fid AND
 	--ON ST_Contains(b.geom, a.geom)
-	ST_Contains(b.geom, a.geom)
+	[b.geom] Contains [a.geom]
 ) WITH DATA;
 
 
