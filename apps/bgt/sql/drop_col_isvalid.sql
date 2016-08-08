a39ece7852b1468cb1650dff610cda3211decedf
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-08-08 10:53:23 +0200

    Extra columns

---
+++ apps/bgt/sql/drop_col_isvalid.sql
@@ -0,0 +1,14 @@
+alter table bgt_begroeidterreindeel drop column col_isvalid;
+alter table bgt_ondersteunendwaterdeel drop column col_isvalid;
+alter table bgt_onbegroeidterreindeel drop column col_isvalid;
+alter table bgt_ondersteunendwegdeel drop column col_isvalid;
+alter table bgt_overbruggingsdeel drop column col_isvalid;
+alter table overbruggingsdeel drop column col_isvalid;
+alter table bgt_overigbouwwerk drop column col_isvalid;
+alter table bgt_pand drop column col_isvalid;
+alter table bgt_scheiding drop column col_isvalid;
+alter table bgt_tunneldeel drop column col_isvalid;
+alter table bgt_waterdeel drop column col_isvalid;
+alter table bgt_wegdeel drop column col_isvalid;
+alter table bgt_paal drop column col_isvalid;
+alter table bgt_kunstwerkdeel drop column col_isvalid;
