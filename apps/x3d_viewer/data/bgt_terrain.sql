c41f15985bb8ae156f2c219df9349531accb418a
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-08-10 11:30:40 +0200

    ST_Dump verified

--- apps/x3d_viewer/data/bgt_terrain.sql
+++ apps/x3d_viewer/data/bgt_terrain.sql
@@ -3,12 +3,12 @@ declare _south decimal(7,1);
 declare _east decimal(7,1);
 declare _north decimal(7,1);
 declare _segmentlength decimal(7,1);
+
 set _west = 93816.0;
 set _east = 93916.0;
 set _south = 463891.0;
 set _north = 463991.0;
 set _segmentlength = 10;
-
 
 with
 bounds AS (
@@ -43,8 +43,8 @@ pointcloud_ground AS (
     x between _west and _east and
     y between _south and _north and
     --ST_Intersects(geom, Geometry(pa))
-	Contains(geom, x, y, z, 28992)
-	--[geom] Contains [x, y, z, 28992]
+	--Contains(geom, x, y, z, 28992)
+	[geom] Contains [x, y, z, 28992]
 ),
 polygons_ AS (
     SELECT NEXT VALUE FOR "counter" as id, ogc_fid as fid, COALESCE(type,'transitie') as type, class, geom
@@ -53,14 +53,14 @@ polygons_ AS (
     SELECT NEXT VALUE FOR "counter" as id, ogc_fid as fid, COALESCE(type,'transitie') as type, class, geom
     FROM bare
 ),
-polygons_dump AS (
-	SELECT parent as id, polygonWKB as geom
-	FROM ST_DUMP((select geom, id from polygons_)) d
+polygons_Dump AS (
+	SELECT parent as id, ST_SetSRID(polygonWKB, 28992) as geom
+	FROM ST_Dump((select geom, id from polygons_)) d
 ),
 polygons AS (
-	SELECT a.*
+	SELECT a.id, fid, type, class, b.geom
 	FROM polygons_ a
-    LEFT JOIN polygons_dump b
+    LEFT JOIN polygons_Dump b
     ON a.id = b.id
 ),
 polygonsz AS (
@@ -74,29 +74,29 @@ polygonsz AS (
 ),
 --insert into _edge_points SELECT cast(path as int) as path, pointg as geom FROM ST_DumpPoints(ST_ExteriorRing(ingeom)) d;
 edge_points AS (
-    SELECT parent as polygon_id, cast(path as int) as path, ST_SetSRID(pointg, 28992) as geom FROM ST_DumpPoints((select geom, id from polygonsz)) d
+    SELECT parent as id, cast(path as int) as path, ST_SetSRID(pointg, 28992) as geom FROM ST_DumpPoints((select geom, id from polygonsz)) d
 ),
 --insert into _emptyz SELECT a.path as path, a.geom as geom , b.z as z, ST_Distance(ST_SetSRID(a.geom, 28992), ST_SetSRID(ST_MakePoint(x, y, z), 28992)) as dist FROM _edge_points a, pointcloud_ground b;
 emptyz AS (
-    --SELECT polygon_id, a.path as path, a.geom as geom , b.z as z, ST_Distance(ST_SetSRID(a.geom, 28992), ST_SetSRID(ST_MakePoint(x, y, z), 28992)) as dist FROM edge_points a, pointcloud_ground b WHERE ST_DWithin(a.geom, x, y, z, 28992, 10)
-    --SELECT polygon_id, a.path as path, a.geom as geom , b.z as z, ST_Distance(a.geom, x, y, z, 28992) as dist FROM edge_points a, pointcloud_ground b WHERE ST_DWithin(a.geom, x, y, z, 28992, 10)
-    SELECT polygon_id, a.path as path, a.geom as geom , b.z as z, ST_Distance(a.geom, x, y, z, 28992) as dist FROM edge_points a, pointcloud_ground b WHERE [a.geom] DWithin [x, y, z, 28992, 10]
+    --SELECT id, a.path as path, a.geom as geom , b.z as z, ST_Distance(ST_SetSRID(a.geom, 28992), ST_SetSRID(ST_MakePoint(x, y, z), 28992)) as dist FROM edge_points a, pointcloud_ground b WHERE ST_DWithin(a.geom, x, y, z, 28992, 10)
+    --SELECT id, a.path as path, a.geom as geom , b.z as z, ST_Distance(a.geom, x, y, z, 28992) as dist FROM edge_points a, pointcloud_ground b WHERE ST_DWithin(a.geom, x, y, z, 28992, 10)
+    SELECT id, a.path as path, a.geom as geom , b.z as z, ST_Distance(a.geom, x, y, z, 28992) as dist FROM edge_points a, pointcloud_ground b WHERE [a.geom] DWithin [x, y, z, 28992, 10]
 ),
 --insert into _ranktest select path, geom, z, dist, RANK() over (PARTITION BY path, geom order by path, dist ASC) as rank from _emptyz;
 ranktest AS (
-    select polygon_id, path, geom, z, dist, RANK() over (PARTITION BY polygon_id, path order by polygon_id, path, dist ASC) as rank from emptyz
+    select id, path, geom, z, dist, RANK() over (PARTITION BY id, path order by id, path, dist ASC) as rank from emptyz
 ),
 --insert into _filledz select path, ST_MakePoint(ST_X(geom), ST_Y(geom), z) as geom from _ranktest where rank = 1 order by path;
 filledz AS (
-    select polygon_id, path, ST_MakePoint(ST_X(geom), ST_Y(geom), z) as geom from ranktest where rank = 1
+    select id, path, ST_MakePoint(ST_X(geom), ST_Y(geom), z) as geom from ranktest where rank = 1
 ),
 --insert into _line_z SELECT ST_MakeLine(geom) as geom FROM _filledz;
 line_z AS (
-    SELECT polygon_id, ST_MakeLine(geom) as geom FROM filledz group by polygon_id
+    SELECT id, ST_MakeLine(geom) as geom FROM filledz group by id
 ),
 basepoints AS (
-	--SELECT polygon_id as id, geom FROM line_z WHERE ST_IsValid(geom)
-	SELECT polygon_id as id, ST_Triangulate2DZ(ST_Collect(geom), 0) as geom FROM line_z
+	--SELECT id as id, geom FROM line_z WHERE ST_IsValid(geom)
+	SELECT id as id, ST_Triangulate2DZ(ST_Collect(geom), 0) as geom FROM line_z
     WHERE
     --ST_IsValid(geom)
     [geom] IsValidD [ST_MakePoint(1.0, 1.0, 1.0)]
@@ -122,4 +122,4 @@ assign_triags AS (
 	AND a.id = b.id
 )
 
-SELECT p.id as id, p.type as type, ST_AsX3D(ST_Collect(p.geom),4.0, 0) as geom FROM assign_triags p GROUP BY id, type;
+SELECT p.id as id, p.type as type, 'grey' as color, ST_AsX3D(ST_Collect(p.geom),4.0, 0) as geom FROM assign_triags p GROUP BY id, type;
