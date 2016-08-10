db1f66703da0f5e1acd736ad1fa72f9ab066f11e
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-08-10 11:39:57 +0200

    ST_Dump verified

--- apps/x3d_viewer/data/bgt_water.sql
+++ apps/x3d_viewer/data/bgt_water.sql
@@ -3,20 +3,17 @@ declare _south decimal(7,1);
 declare _east decimal(7,1);
 declare _north decimal(7,1);
 declare _segmentlength decimal(7,1);
+
 set _west = 93816.0;
 set _east = 93916.0;
 set _south = 463891.0;
 set _north = 463991.0;
 set _segmentlength = 10;
 
-
 DROP SEQUENCE "counter";
 CREATE SEQUENCE "counter" AS INTEGER;
 
-DROP SEQUENCE "polygon_id";
-CREATE SEQUENCE "polygon_id" AS INTEGER;
-
- with
+with
 bounds AS (
 	SELECT ST_Segmentize(ST_MakeEnvelope(_west, _south, _east, _north, 28992),_segmentlength) as geom
 ),
@@ -37,40 +34,40 @@ pointcloud_water AS (
 terrain_ AS (
 	SELECT NEXT VALUE FOR "counter" as id, ogc_fid as fid, plus_type as typ, 'water' as class, ST_Intersection(a.wkt, b.geom) as geom FROM  bgt_waterdeel a, bounds b WHERE [a.wkt] Intersects [b.geom]
 ),
-terrain_dump AS (
+terrain_Dump AS (
 	SELECT parent as id, polygonWKB as geom FROM ST_Dump((select geom, id from terrain_)) a
 ),
 terrain AS (
-	SELECT a.id, a.fid, a.typ, a.class, b.geom FROM terrain_ a LEFT JOIN terrain_dump b ON a.id = b.id
+	SELECT a.id, a.fid, a.typ, a.class, b.geom FROM terrain_ a LEFT JOIN terrain_Dump b ON a.id = b.id
 ),
 polygons AS (
-	SELECT NEXT VALUE for "polygon_id" as polygon_id, * FROM terrain
+	SELECT * FROM terrain
     WHERE
     --ST_GeometryType(geom) = 'ST_Polygon'
     [geom] IsType ['ST_Polygon']
 ),
 polygonsz AS (
-	SELECT a.id, a.fid, polygon_id, a.typ, a.class, ST_Translate(ST_Force3D(a.geom), 0,0,0) as geom --fixed level
+	SELECT a.id, a.fid, a.typ, a.class, ST_Translate(ST_Force3D(a.geom), 0,0,0) as geom --fixed level
 	FROM polygons a
 	--GROUP BY a.id, a.fid, a.typ, a.class, a.geom
 ),
 basepoints AS (
-	SELECT id, polygon_id, geom FROM polygonsz
+	SELECT id, fid, geom FROM polygonsz
     WHERE
     --ST_IsValid(geom)
     [geom] IsValidD [ST_MakePoint(1.0, 1.0, 1.0)]
 ),
 triangles_b AS (
-    select polygon_id, id, ST_Triangulate2DZ(ST_Collect(geom), 0) as geom from basepoints group by polygon_id, id
+    select id, fid, ST_Triangulate2DZ(ST_Collect(geom), 0) as geom from basepoints group by id, fid
 ),
-triangles_dump AS (
-    SELECT parent as polygon_id, ST_MakePolygon(ST_ExteriorRing( a.polygonWKB)) as geom FROM ST_Dump((select geom, polygon_id from triangles_b)) a
+triangles_Dump AS (
+    SELECT parent as id, ST_MakePolygon(ST_ExteriorRing( a.polygonWKB)) as geom FROM ST_Dump((select geom, id from triangles_b)) a
 ),
 triangles AS (
-    SELECT a.polygon_id, a.id, b.geom
+    SELECT a.id, a.fid, b.geom
     FROM triangles_b a
-    LEFT JOIN triangles_dump b
-    ON a.polygon_id = b.polygon_id
+    LEFT JOIN triangles_Dump b
+    ON a.id = b.id
 ),
 assign_triags AS (
 	SELECT 	a.*, b.typ, b.class
@@ -82,6 +79,6 @@ assign_triags AS (
     WHERE
     --ST_Intersects(ST_Centroid(b.geom), c.geom)
     [ST_Centroid(b.geom)] Intersects [c.geom]
-	AND a.polygon_id = b.polygon_id
+	AND a.id = b.id
 )
 SELECT p.id AS id, 'water' as type, ST_AsX3D(ST_Collect(p.geom),4.0, 0) as geom FROM assign_triags p GROUP BY id, type;
