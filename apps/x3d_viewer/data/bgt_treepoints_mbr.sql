c9979b5681175e744371002c11b24cfcfafe5306
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-10-10 17:30:57 +0200

    Some cleanning

--- apps/x3d_viewer/data/bgt_treepoints_mbr.sql
+++ apps/x3d_viewer/data/bgt_treepoints_mbr.sql
@@ -1,14 +1,5 @@
-declare _west decimal(7,1);
-declare _south decimal(7,1);
-declare _east decimal(7,1);
-declare _north decimal(7,1);
-declare _segmentlength decimal(7,1);
 
-set _west = 93816.0;
-set _east = 93916.0;
-set _south = 463891.0;
-set _north = 463991.0;
-set _segmentlength = 10;
+
 
 
 
