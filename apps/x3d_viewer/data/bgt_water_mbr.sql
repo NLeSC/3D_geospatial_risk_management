956539deb9e811f81dead08b98818ae286f0edbd
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-10-10 17:32:46 +0200

    Do not use filter joins with INNER join. Set SRID. Some cleanning.

--- apps/x3d_viewer/data/bgt_water_mbr.sql
+++ apps/x3d_viewer/data/bgt_water_mbr.sql
@@ -1,19 +1,9 @@
-declare _west decimal(7,1);
-declare _south decimal(7,1);
-declare _east decimal(7,1);
-declare _north decimal(7,1);
-declare _segmentlength decimal(7,1);
 
-set _west = 93816.0;
-set _east = 93916.0;
-set _south = 463891.0;
-set _north = 463991.0;
-set _segmentlength = 10;
 
 
 with
 bounds AS (
-	SELECT ST_Segmentize(ST_MakeEnvelope(_west, _south, _east, _north, 28992),_segmentlength) as geom
+	SELECT ST_SetSRID(ST_Segmentize(ST_MakeEnvelope(_west, _south, _east, _north, 28992),_segmentlength), 28992) as geom
 ),
 pointcloud_water AS (
 	SELECT
@@ -29,7 +19,7 @@ pointcloud_water AS (
         and c = 9
 ),
 terrain_ AS (
-	SELECT NEXT VALUE FOR "counter" as id, ogc_fid as fid, plus_type as typ, 'water' as class, ST_Intersection(a.wkt, b.geom) as geom 
+	SELECT NEXT VALUE FOR "counter" as id, ogc_fig as fid, plus_type as typ, 'water' as class, ST_Intersection(a.wkt, b.geom) as geom
     FROM  bgt_waterdeel a, bounds b
     WHERE
     (NOT
@@ -44,7 +34,7 @@ terrain_Dump AS (
 	SELECT parent as id, polygonWKB as geom FROM ST_Dump((select geom, id from terrain_)) a
 ),
 terrain AS (
-	SELECT a.id, a.fid, a.typ, a.class, b.geom FROM terrain_ a LEFT JOIN terrain_Dump b ON a.id = b.id
+	SELECT a.id, a.fid, a.typ, a.class, ST_SetSRID(b.geom, 28992) as geom FROM terrain_ a LEFT JOIN terrain_Dump b ON a.id = b.id
 ),
 polygons AS (
 	SELECT * FROM terrain
@@ -79,12 +69,14 @@ assign_triags AS (
 	SELECT 	a.*, b.typ, b.class
 	FROM triangles a
 	INNER JOIN polygons b
-	--ON ST_Contains(ST_SetSRID(b.geom, 28992), ST_SetSRID(a.geom, 28992))
-	ON [ST_SetSRID(b.geom, 28992)] Contains [ST_SetSRID(a.geom, 28992)]
+	ON ST_Contains(ST_SetSRID(b.geom, 28992), ST_SetSRID(a.geom, 28992))
+	--ON [ST_SetSRID(b.geom, 28992)] Contains [ST_SetSRID(a.geom, 28992)]
+	--, polygons b
 	, bounds c
     WHERE
     --ST_Intersects(ST_Centroid(b.geom), c.geom)
     [ST_Centroid(b.geom)] Intersects [c.geom]
+	--AND [ST_SetSRID(b.geom, 28992)] Contains [ST_SetSRID(a.geom, 28992)]
 	AND a.id = b.id
 )
 SELECT p.id AS id, 'water' as type, 'blue' as color, ST_AsX3D(ST_Collect(p.geom),4.0, 0) as geom FROM assign_triags p GROUP BY id, type;
