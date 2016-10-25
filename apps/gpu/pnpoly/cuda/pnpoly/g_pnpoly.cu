b99278e65b8e2540b44864ec7d75907534e3ea6f
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-10-25 16:44:04 +0200

    Add Ben's version of pnpoly.

--- apps/gpu/pnpoly/cuda/pnpoly/g_pnpoly.cu
+++ apps/gpu/pnpoly/cuda/pnpoly/g_pnpoly.cu
@@ -10,8 +10,113 @@ extern "C" {
 #include "g_pnpoly.h"
 }
 
+#define VERTICES 2000
+
+#ifndef between_method
+#define between_method 1
+#endif
+
+#ifndef use_method
+#define use_method 1
+#endif
+
+#ifndef block_size_x
+#define block_size_x 256
+#endif
+#ifndef block_size_y
+#define block_size_y 1
+#endif
+#ifndef block_size_z
+#define block_size_z 1
+#endif
+
+#ifndef tile_size
+#define tile_size 1
+#endif
+
+#define max_streams 1
+__constant__ float d_verticesX[VERTICES];
+__constant__ float d_verticesY[VERTICES];
+__constant__ float d_slopes[VERTICES];
+
+__device__ __forceinline__ int is_between(float a, float b, float c) {
+    #if between_method == 0
+        return (b > a) != (c > a);
+    #elif between_method == 1
+        return ((b <= a) && (c > a)) || ((b > a) && (c <= a));
+    #elif between_method == 2
+        return ((a - b) == 0.0f) || ((a - b) * (a - c) < 0.0f);
+    #elif between_method == 3
+        //Interestingly enough method 3 exactly the same as method 2, only in a different order.
+        //the performance difference between method 2 and 3 can be huge depending on all the other optimization parameters.
+        return ((a - b) * (a - c) < 0.0f) || (a - b == 0.0f);
+    #endif
+}
+
+__global__ void cn_pnpolyBEN(char* bitmap, float *px, float *py, int npoints, int nverts) {
+    int i = blockIdx.x * block_size_x * tile_size + threadIdx.x;
+    if (i < npoints) {
+
+        char c[tile_size];
+        float2 lpoints[tile_size];
+        #pragma unroll
+        for (int ti=0; ti<tile_size; ti++) {
+            c[ti] = 0;
+            lpoints[ti] = make_float2(px[i+block_size_x*ti], py[i+block_size_x*ti]);
+        }
+
+        int k = nverts-1;
+
+        for (int j=0; j<nverts; k = j++) {    // edge from vj to vk
+            float2 vj = make_float2(d_verticesX[j], d_verticesY[j]); 
+            float2 vk = make_float2(d_verticesX[k], d_verticesY[k]); 
+
+            #if use_precomputed_slopes == 0
+            float slope = (vk.x-vj.x) / (vk.y-vj.y);
+            #elif use_precomputed_slopes == 1
+            float slope = d_slopes[j];
+            #endif
+
+            #pragma unroll
+            for (int ti=0; ti<tile_size; ti++) {
+
+                float2 p = lpoints[ti];
+
+                #if use_method == 0
+                if ( is_between(p.y, vj.y, vk.y) &&         //if p is between vj and vk vertically
+                     (p.x < slope * (p.y-vj.y) + vj.x) ) {  //if p.x crosses the line vj-vk when moved in positive x-direction
+                    c[ti] = !c[ti];
+                }
+
+                #elif use_method == 1
+                //Same as method 0, but attempts to reduce divergence by avoiding the use of an if-statement.
+                //Whether this is more efficient is data dependent because there will be no divergence using method 0, when none
+                //of the threads within a warp evaluate is_between as true
+                int b = is_between(p.y, vj.y, vk.y);
+                c[ti] += b && (p.x < vj.x + slope * (p.y - vj.y));
+
+                #endif
+
+
+            }
+
+        }
+
+        #pragma unroll
+        for (int ti=0; ti<tile_size; ti++) {
+            //could do an if statement here if 1s are expected to be rare
+            #if use_method == 0
+            bitmap[i+block_size_x*ti] = c[ti];
+            #elif use_method == 1
+            bitmap[i+block_size_x*ti] = c[ti] & 1;
+            #endif
+        }
+    }
+}
+
+
 // Cuda version
-__global__ void pnpoly_cnGPU(const float *px, const float *py, const float *vx, const float *vy, char* cs, int npoint, int nvert)
+__global__ void pnpoly_cnGPU(char *cs, const float *px, const float *py, const float *vx, const float *vy, int npoint, int nvert)
 {
     extern __shared__ int s[];
     float *tvx = (float*) s;
@@ -134,26 +239,28 @@ void reset_GPU() {
 
 int pnpoly_GPU(signed char **mc, int nvert, int npoint, float *mpx, float *mpy, float *mvx, float *mvy) {
     /*GPU*/
-    bool bPinGenericMemory = true;                 // Allocate generic memory with malloc() and pin it later instead of using cudaHostAlloc()
+    bool bPinGenericMemory = false;                 // Allocate generic memory with malloc() and pin it later instead of using cudaHostAlloc()
     unsigned int flags;
     size_t pbytes, vbytes, cbytes;
     float *px, *py, *vx, *vy;
-    //float *px_UA, *py_UA, *vx_UA, *vy_UA;          // Non-4K Aligned Pinned memory on the CPU
     float *d_px, *d_py, *d_vx, *d_vy;              // Device pointers for mapped memory
     char *c, *c_UA, *d_c;                            // Device pointers for mapped memory
     struct timeval stop, start;
     unsigned long long t;
     int i, count = 0;
+    float *h_slopes;
+    cudaError_t err;
 
+    //create CUDA streams and events
+    cudaStream_t stream[max_streams];
+    for (int i=0; i<max_streams; i++) {
+        err = cudaStreamCreate(&stream[i]);
+        if (err != cudaSuccess) {
+            fprintf(stderr, "Error in cudaStreamCreate: %s\n", cudaGetErrorString(err));
+        }
+    }
     /*CUDA monitoring*/
     cudaEvent_t cstart, cstop;
-
-	/*
-	if (GPU_SETUP != 1) {
-        printf("> GPU_SETUP was not initialized.\n");
-		setup_GPU();
-	}
-	*/
 
     if (bPinGenericMemory) {
 	/*Return str with the error*/
@@ -168,30 +275,16 @@ int pnpoly_GPU(signed char **mc, int nvert, int npoint, float *mpx, float *mpy, 
     cbytes = npoint*sizeof(char);
     printf("Bytes allocated for npoints %d and nvert %d: pbytes %zu, vbytes %zu, cbytes %zu\n", npoint, nvert, pbytes, vbytes, cbytes);
 
+    err = cudaHostAlloc((void **)&h_slopes, nvert*sizeof(float), cudaHostAllocMapped);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaHostAlloc: %s\n", cudaGetErrorString(err));
+    }
+
     if (bPinGenericMemory) {
 #if CUDART_VERSION >= 4000
         gettimeofday(&start, NULL);
-        /*
-        px_UA = (float *) malloc(pbytes + MEMORY_ALIGNMENT);
-        py_UA = (float *) malloc(pbytes + MEMORY_ALIGNMENT);
-        vx_UA = (float *) malloc(vbytes + MEMORY_ALIGNMENT);
-        vy_UA = (float *) malloc(vbytes + MEMORY_ALIGNMENT);
-        c_UA = (char *) malloc(cbytes + MEMORY_ALIGNMENT);
-
-        // We need to ensure memory is aligned to 4K (so we will need to padd memory accordingly)
-        px = (float *) ALIGN_UP(px_UA, MEMORY_ALIGNMENT);
-        py = (float *) ALIGN_UP(py_UA, MEMORY_ALIGNMENT);
-        vx = (float *) ALIGN_UP(vx_UA, MEMORY_ALIGNMENT);
-        vy = (float *) ALIGN_UP(vy_UA, MEMORY_ALIGNMENT);
-        */
         c_UA = (char *) malloc(cbytes + MEMORY_ALIGNMENT);
         c = (char *) ALIGN_UP(c_UA, MEMORY_ALIGNMENT);
-		/*
-        px = (float *) ALIGN_UP(mpx, MEMORY_ALIGNMENT);
-        py = (float *) ALIGN_UP(mpy, MEMORY_ALIGNMENT);
-        vx = (float *) ALIGN_UP(mvx, MEMORY_ALIGNMENT);
-        vy = (float *) ALIGN_UP(mvy, MEMORY_ALIGNMENT);
-        */
 
         px = mpx;
         py = mpy;
@@ -215,18 +308,41 @@ int pnpoly_GPU(signed char **mc, int nvert, int npoint, float *mpx, float *mpy, 
         checkCudaErrors(cudaHostAlloc((void **)&vx, vbytes, flags));
         checkCudaErrors(cudaHostAlloc((void **)&vy, vbytes, flags));
         checkCudaErrors(cudaHostAlloc((void **)&c, cbytes, flags));
+
+		/*Copy point and vertices*/
+    	gettimeofday(&start, NULL);
+		memcpy(px, mpx, pbytes); 
+		memcpy(py, mpy, pbytes); 
+		memcpy(vx, mvx, vbytes); 
+		memcpy(vy, mvy, vbytes); 
+    	gettimeofday(&stop, NULL);
+    	t = 1000 * (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1000;
+    	printf("MemCopy took %llu ms\n", t);
+
 #endif
     }
 
-	/*Copy point and vertices*/
-    gettimeofday(&start, NULL);
-	//memcpy(px, mpx, pbytes); 
-	//memcpy(py, mpy, pbytes); 
-	//memcpy(vx, mvx, vbytes); 
-	//memcpy(vy, mvy, vbytes); 
-    gettimeofday(&stop, NULL);
-    t = 1000 * (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1000;
-    printf("MemCopy took %llu ms\n", t);
+    //transfer vertices to d_vertices
+    err = cudaMemcpyToSymbolAsync(d_verticesX, vx, nvert*sizeof(float), 0, cudaMemcpyHostToDevice, stream[0]);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaMemcpyToSymbolAsync: %s\n", cudaGetErrorString(err));
+    }
+    err = cudaMemcpyToSymbolAsync(d_verticesY, vy, nvert*sizeof(float), 0, cudaMemcpyHostToDevice, stream[0]);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaMemcpyToSymbolAsync: %s\n", cudaGetErrorString(err));
+    }
+
+    #if use_precomputed_slopes == 1
+    //precompute the slopes and transfer to symbol d_slopes
+    h_slopes[0] = (vx[nvert-1] - vx[0]) / (vy[nvert-1] - vy[0]);
+    for (int i=1; i<nvert; i++) {
+        h_slopes[i] = (vx[i-1] - vx[i]) / (vy[i-1] - vy[i]);
+    }
+    err = cudaMemcpyToSymbolAsync(d_slopes, h_slopes, nvert*sizeof(float), 0, cudaMemcpyHostToDevice, stream[0]);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in cudaMemcpyToSymbolAsync: %s\n", cudaGetErrorString(err));
+    }
+    #endif
 
     /* Get the device pointers for the pinned CPU memory mapped into the GPU
        memory space. */
@@ -247,19 +363,16 @@ int pnpoly_GPU(signed char **mc, int nvert, int npoint, float *mpx, float *mpy, 
     //dim3 block(256);
     dim3 block(BLOCK_SIZE);
     dim3 grid((unsigned int)ceil(npoint/(float)block.x));
-    //vectorAddGPU<<<grid, block>>>(d_a, d_b, d_c, nelem);
-    //gettimeofday(&start, NULL);
-    
-    //pnpoly_cnGPU<<<grid, block>>>(d_px, d_py, d_vx, d_vy, d_c, npoint, nvert);
-    //size_t sh_size = 2*607*sizeof(float);
-    //pnpoly_cnGPU<<<grid, block,sh_size>>>(d_px, d_py, d_vx, d_vy, d_c, npoint, nvert);
 
     cudaEventCreate(&cstart);
     cudaEventCreate(&cstop);
-    cudaEventRecord(cstart);
-    size_t sh_size = 2*nvert*sizeof(float);
-    pnpoly_cnGPU<<<grid, block, sh_size>>>(d_px, d_py, d_vx, d_vy, d_c, npoint, nvert);
-    cudaEventRecord(cstop);
+    cudaEventRecord(cstart, stream[0]);
+    //size_t sh_size = 2*nvert*sizeof(float);
+    //pnpoly_cnGPU<<<grid, block, sh_size>>>(d_c, d_px, d_py, d_vx, d_vy, npoint, nvert);
+
+    dim3 threads(block_size_x, block_size_y, block_size_z);
+	cn_pnpolyBEN<<<grid, threads, 0, stream[0]>>>(d_c, d_px, d_py, npoint, nvert);
+    cudaEventRecord(cstop, stream[0]);
 
     checkCudaErrors(cudaDeviceSynchronize());
     getLastCudaError("pnpoly_cnGPU() execution failed");
@@ -293,10 +406,6 @@ int pnpoly_GPU(signed char **mc, int nvert, int npoint, float *mpx, float *mpy, 
         checkCudaErrors(cudaHostUnregister(vx));
         checkCudaErrors(cudaHostUnregister(vy));
         checkCudaErrors(cudaHostUnregister(c));
-    //    free(px_UA);
-    //    free(py_UA);
-    //    free(vx_UA);
-    //    free(vy_UA);
         free(c_UA);
 #endif
     } else {
@@ -307,6 +416,10 @@ int pnpoly_GPU(signed char **mc, int nvert, int npoint, float *mpx, float *mpy, 
         checkCudaErrors(cudaFreeHost(vy));
         checkCudaErrors(cudaFreeHost(c));
 #endif
+    }
+    cudaFreeHost(h_slopes);
+    for (int i=0; i<max_streams; i++) {
+        err = cudaStreamDestroy(stream[i]);
     }
     gettimeofday(&stop, NULL);
     t = 1000 * (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1000;
